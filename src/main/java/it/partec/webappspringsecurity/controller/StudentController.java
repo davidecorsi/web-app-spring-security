4433e0476f81c6339f69ec476d95ef569cd6a936
Davide Corsi <davide.corsi@par-tec.it>
2021-09-24 13:04:29 +0200

    database

--- src/main/java/it/partec/webappspringsecurity/controller/StudentController.java
+++ src/main/java/it/partec/webappspringsecurity/controller/StudentController.java
@@ -2,12 +2,9 @@ package it.partec.webappspringsecurity.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,99 +17,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.partec.webappspringsecurity.dto.StudentDto;
-import it.partec.webappspringsecurity.service.StudentService;
+import it.partec.webappspringsecurity.service.impl.StudentServiceImpl;
 
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-	
+
 	@Autowired
-	private StudentService studentService;
-	
+	private StudentServiceImpl studentService;
+
 	@GetMapping
-	@Secured("ROLE_USER")
-	public ResponseEntity<List<StudentDto>> getListStudent(Authentication authentication) {
+	public ResponseEntity<List<StudentDto>> getListStudent(Authentication authentication) throws Exception {
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		List<StudentDto> studentList = null;
-		try {
-			studentList = studentService.getListStudent();
-		} catch(Exception e) {
-			return new ResponseEntity<List<StudentDto>>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		studentList = studentService.getListStudent();
 		return new ResponseEntity<List<StudentDto>>(studentList, HttpStatus.OK);
 	}
-	
-	/*
-	 * @GetMapping public ResponseEntity<List<StudentDto>> getSearchStudent(
-	 * 
-	 * @RequestParam(name = "name", required = false) String name,
-	 * 
-	 * @RequestParam(name = "surname", required = false) String surname,
-	 * 
-	 * @RequestParam(name = "age", required = false) Long age,
-	 * 
-	 * @RequestParam(name = "className", required = false) String className) {
-	 * StudentDto studentDto = new StudentDto(); studentDto.setName(name);
-	 * studentDto.setSurname(surname); studentDto.setAge(age);
-	 * studentDto.setClassName(className); List<StudentDto> studentList = null; try
-	 * { studentList = studentService.searchStudent(studentDto); } catch(Exception
-	 * e) { return new
-	 * ResponseEntity<List<StudentDto>>(HttpStatus.INTERNAL_SERVER_ERROR); } return
-	 * new ResponseEntity<List<StudentDto>>(studentList, HttpStatus.OK); }
-	 */
-	
+
+
+	//	@GetMapping public ResponseEntity<List<StudentDto>> getSearchStudent(
+	//			@RequestParam(name = "name", required = false) String name,
+	//			@RequestParam(name = "surname", required = false) String surname,
+	//			@RequestParam(name = "age", required = false) Long age,
+	//			@RequestParam(name = "className", required = false) String className) throws Exception {
+	//		StudentDto studentDto = new StudentDto(); studentDto.setName(name);
+	//		studentDto.setSurname(surname); studentDto.setAge(age);
+	//		studentDto.setClassName(className); 
+	//		List<StudentDto> studentList = null; 
+	//		studentList = studentService.searchStudent(studentDto);
+	//		return new ResponseEntity<List<StudentDto>>(studentList, HttpStatus.OK); 
+	//	}
+
+
 	@GetMapping("/{id}")
-	@Secured("ROLE_USER")
-	public ResponseEntity<StudentDto> getStudent(Authentication authentication, @PathVariable("id") long id) {
+	public ResponseEntity<StudentDto> getStudent(Authentication authentication, @PathVariable("id") long id) throws Exception {
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		StudentDto studentDto = null;
-		try {
-			studentDto = studentService.getStudent(id);
-		} catch(Exception e) {
-			return new ResponseEntity<StudentDto>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		if(studentDto != null) {
-			return new ResponseEntity<StudentDto>(studentDto, HttpStatus.OK);
-		}
-		return new ResponseEntity<StudentDto>(HttpStatus.NOT_FOUND);
+		studentDto = studentService.getStudent(id);
+		return new ResponseEntity<StudentDto>(studentDto, HttpStatus.OK);
 	}
-	
+
 	@PostMapping
-	@Secured("ROLE_ADMIN")
-	public ResponseEntity<Object> addStudent(Authentication authentication, @RequestBody StudentDto studentDto) {
+	public ResponseEntity<Object> addStudent(Authentication authentication, @RequestBody StudentDto studentDto) throws Exception {
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		try {
-			studentService.addStudent(studentDto);
-		} catch(Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		studentService.addStudent(studentDto);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	@Secured("ROLE_ADMIN")
-	public ResponseEntity<Object> deleteStudent(Authentication authentication, @PathVariable("id") long id) {
+	public ResponseEntity<Object> deleteStudent(Authentication authentication, @PathVariable("id") long id) throws Exception {
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		try {
-			studentService.deleteStudent(id);
-		} catch(Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		studentService.deleteStudent(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
-	@PutMapping
-	@Secured("ROLE_ADMIN")
-	public ResponseEntity<Object> updateStudent(Authentication authentication, @RequestBody StudentDto studentDto) {
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateStudent(Authentication authentication, @PathVariable("id") long id, @RequestBody StudentDto studentDto) throws Exception {
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		try {
-			studentService.updateStudent(studentDto);
-		} catch(EntityNotFoundException e) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		} catch(Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		studentService.updateStudent(id, studentDto);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
